cf9fd99ce60aea57598d131c83b551577991f3e4
allan.kleber <allan.kleber@esig.com.br>
2020-06-11 20:59:09 -0300

    app primeiro commit

---
+++ InglesCriancas/InglesCriancas/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InglesCriancas
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btCores_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageCores());
+        }
+
+        private void btNumeros_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageNumeros());
+        }
+
+        private void btFamilia_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageFamilia());
+
+        }
+
+        private void btSair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InglesCriancas/InglesCriancas/PageCores.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InglesCriancas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageCores : ContentPage
+    {
+        public PageCores()
+        {
+            InitializeComponent();
+        }
+    }
+}
